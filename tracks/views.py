9f5399c54d51e00314c0848abe2e6610202f7d9e
Alberto L <Alberto@MacBook-Pro-de-Alberto.local>
2015-06-08 14:31:39 -0600

    Agregando Furmulario para agregar usuario

--- tracks/views.py
+++ tracks/views.py
@@ -1,3 +1,5 @@
+import json
+
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404
 
@@ -12,4 +14,21 @@ def track_view(request, title):
 
 	track = get_object_or_404(Track, title=title)
 	bio= track.artist.biography
-	return render(request, 'track.html',{'track':track, 'bio': bio})
+
+	data = {
+		'title': track.title,
+		'order': track.order,
+		'album': track.album.title,
+		'artist': {
+			'name': track.artist.first_name,
+			'bio': bio,
+		}
+	}
+	#convertir de diccionario a json
+	json_data = json.dumps(data)
+
+	#convertir de json a diccionario
+	#dic = json.loads(string_json)
+
+	return HttpResponse(json_data, content_type='application/json')
+	#return render(request, 'track.html',{'track':track, 'bio': bio})
