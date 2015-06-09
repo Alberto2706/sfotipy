aa79d2d9ced42b990b8e0ac0d78d8d7846ecb6b3
Alberto L <Alberto@MacBook-Pro-de-Alberto.local>
2015-06-08 22:27:15 -0600

    Agregando formulario para login por medio de email y password

--- userprofiles/views.py
+++ userprofiles/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from .forms import UserCreationEmailForm
+from .forms import UserCreationEmailForm, EmailAuthenticationForm
+from django.contrib.auth import login
 
 # Create your views here.
 def signup(request):
@@ -9,3 +10,11 @@ def signup(request):
         form.save()
 
     return render(request, 'signup.html', {'form': form})
+
+def signin(request):
+    form = EmailAuthenticationForm(request.POST or None)
+
+    if form.is_valid():
+        login(request,form.get_user())
+
+    return render(request, 'signin.html', {'form':form})
